cb26c01a57dd74d6eedbc86565c44fb820c87d10
Ricklus000 <albin.rissler@gmail.com>
2023-01-18 14:27:11 +0100

    boss

--- Assets/Scripts/boss.cs
+++ Assets/Scripts/boss.cs
@@ -98,13 +98,6 @@ public class boss : MonoBehaviour
         }
     }
    
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag=="bullet")
-        {
-            bosshealth.currenthealth -= 1;
-        }
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
