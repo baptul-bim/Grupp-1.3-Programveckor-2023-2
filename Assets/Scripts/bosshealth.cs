e25ea6c2c82bbab909d122a760b1b5ef76183eef
Ricklus000 <albin.rissler@gmail.com>
2023-01-18 14:19:24 +0100

    boss försökfix

--- Assets/Scripts/bosshealth.cs
+++ Assets/Scripts/bosshealth.cs
@@ -5,12 +5,13 @@ using UnityEngine.UI;
 public class bosshealth : MonoBehaviour
 {
     private Image health;
-    public float currenthealth = 20f;
-    public float maxhealth = 50f;
+    public float currenthealth;
+    public float maxhealth = 20f;
     // Start is called before the first frame update
     void Start()
     {
         health = GetComponent<Image>();
+        currenthealth = maxhealth;
     }
 
     // Update is called once per frame
