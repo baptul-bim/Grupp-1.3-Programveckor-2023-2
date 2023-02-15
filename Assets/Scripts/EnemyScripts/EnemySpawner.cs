93a02e54f3f6d08d597f1212b44ea8f452889390
baptul-bim <baptul.bim@gmail.com>
2023-02-15 15:27:19 +0100

    awdiybguahgywdgyajwbvudkb8otfaYI

--- Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -11,6 +11,10 @@ public class EnemySpawner : MonoBehaviour
 
     public GameObject[] enemyPrefab;
 
+    Vector3 spawnPlace;
+    public Vector2 direction;
+    Vector3 fromGround;
+
     [SerializeField]
     GameObject axePrefab;
     [SerializeField]
@@ -23,7 +27,7 @@ public class EnemySpawner : MonoBehaviour
 
     int enemyToSpawn;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -35,57 +39,67 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //while (enemyPoints < 1)
-        //{
-            if (enemyPoints == 1)
+
+        if (enemyPoints == 1)
+        {
+            spawnRand();
+            enemyToSpawn = 0;
+            enemyPoints -= (enemyToSpawn + 1);
+
+            Instantiate(axePrefab, spawnPlace, Quaternion.identity);
+        }
+        else if (enemyPoints == 2)
+        {
+            int randEnemy = Random.Range(0, 2);
+            enemyToSpawn = randEnemy;
+            if (randEnemy == 0)
             {
+                spawnRand();
                 enemyToSpawn = 0;
                 enemyPoints -= (enemyToSpawn + 1);
-                Instantiate(axePrefab, new Vector3(Random.Range(-2.4f, 2.4f) + LevelLoop.nextRoomPos, 2, 0), Quaternion.identity);
-            }
-            else if (enemyPoints == 2)
-            {
-                int randEnemy = Random.Range(0, 2);
-                enemyToSpawn = randEnemy;
-                if (randEnemy == 0)
-                {
-                    enemyToSpawn = 0;
-                    enemyPoints -= (enemyToSpawn + 1);
 
-                    Instantiate(axePrefab, new Vector3(Random.Range(-2.4f, 2.4f) + LevelLoop.nextRoomPos, 2, 0), Quaternion.identity);
+                Instantiate(axePrefab, spawnPlace, Quaternion.identity);
 
-                }
-                if (randEnemy == 1)
-                {
-                    enemyToSpawn = 1;
-                    enemyPoints -= (enemyToSpawn + 1);
-                    Instantiate(chainsawPrefab, new Vector3(Random.Range(-2.4f, 2.4f) + LevelLoop.nextRoomPos, 2, 0), Quaternion.identity);
-                }
             }
-            else if (enemyPoints >= 3)
+            if (randEnemy == 1)
             {
-                int randEnemy = Random.Range(0, 3);
-                enemyToSpawn = randEnemy;
-                if (randEnemy == 0)
-                {
-                    enemyToSpawn = 0;
-                    enemyPoints -= (enemyToSpawn + 1);
-                    Instantiate(axePrefab, new Vector3(Random.Range(-2.4f, 2.4f) + LevelLoop.nextRoomPos, 2, 0), Quaternion.identity);
-                }
-                if (randEnemy == 1)
-                {
-                    enemyToSpawn = 1;
-                    enemyPoints -= (enemyToSpawn + 1);
-                    Instantiate(chainsawPrefab, new Vector3(Random.Range(-2.4f, 2.4f) + LevelLoop.nextRoomPos, 2, 0), Quaternion.identity);
-                }
-                if (randEnemy == 2)
-                {
-                    enemyToSpawn = 2;
-                    enemyPoints -= (enemyToSpawn + 1);
-                    Instantiate(flamethrowerPrefab, new Vector3(Random.Range(-2.4f, 2.4f) + LevelLoop.nextRoomPos, 2, 0), Quaternion.identity);
-                }
+                spawnRand();
+                enemyToSpawn = 1;
+                enemyPoints -= (enemyToSpawn + 1);
+
+                Instantiate(chainsawPrefab, spawnPlace, Quaternion.identity);
             }
-        //}
+        }
+        else if (enemyPoints >= 3)
+        {
+            int randEnemy = Random.Range(0, 3);
+            enemyToSpawn = randEnemy;
+            if (randEnemy == 0)
+            {
+                spawnRand();
+                enemyToSpawn = 0;
+                enemyPoints -= (enemyToSpawn + 1);
+
+                Instantiate(axePrefab, spawnPlace, Quaternion.identity);
+            }
+            if (randEnemy == 1)
+            {
+                spawnRand();
+                enemyToSpawn = 1;
+                enemyPoints -= (enemyToSpawn + 1);
+
+                Instantiate(chainsawPrefab, spawnPlace, Quaternion.identity);
+            }
+            if (randEnemy == 2)
+            {
+                spawnRand();
+                enemyToSpawn = 2;
+                enemyPoints -= (enemyToSpawn + 1);
+
+                Instantiate(flamethrowerPrefab, spawnPlace, Quaternion.identity);
+            }
+        }
+
 
     }
 
@@ -93,12 +107,27 @@ public class EnemySpawner : MonoBehaviour
     {
         enemyPoints = enemyPointsIncrease + 0.5f;
         enemyPointsIncrease = enemyPoints;
-        
-        
+
+
 
 
 
     }
 
+    public void spawnRand()
+    {
+        spawnPlace = new Vector3(Random.Range(-2.4f, 2.4f) + LevelLoop.nextRoomPos, 2, 0);
+        RaycastHit2D groundRay = Physics2D.Raycast(spawnPlace, direction);
+        Debug.DrawRay(spawnPlace, direction);
+
+
+
+        if (groundRay.collider != null && groundRay.transform.tag == ("Ground"))
+        {
+            fromGround = new Vector3(0f, groundRay.distance, 0f);
+            spawnPlace.y -= groundRay.distance;
+        }
+        Debug.Log(spawnPlace);
+    }
 
 }
