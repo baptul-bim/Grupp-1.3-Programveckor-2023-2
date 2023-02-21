fc2dffccd524eca907153962028912558fd83f20
lillamyy <114159749+lillamyy@users.noreply.github.com>
2023-02-21 08:52:31 +0100

    nn

--- Assets/tocredits.cs
+++ Assets/tocredits.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class tocredits : MonoBehaviour
 {
+    //clara
     private void OnEnable()
     {
         SceneManager.LoadScene("Credits", LoadSceneMode.Single);
