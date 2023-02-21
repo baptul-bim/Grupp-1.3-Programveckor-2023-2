b375a490ff6e432b2c28d1b9f51ee754f1cc8d07
maja345eee <121087018+maja345eee@users.noreply.github.com>
2023-02-21 10:30:29 +0100

    kommentarer

--- Assets/Scripts/nextSceneBoss.cs
+++ Assets/Scripts/nextSceneBoss.cs
@@ -16,7 +16,7 @@ public class nextSceneBoss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (LevelLoop.roomAmount == 20) //Maja
+        //if (LevelLoop.roomAmount == 20) //Maja, gammal version av levelLoop och bossRoom scen
         //{
             //SceneManager.LoadScene("BossRoom");
             //temp.temptimer = 99999;
